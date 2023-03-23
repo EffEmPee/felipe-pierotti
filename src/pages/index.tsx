fe3c13efc8bd6b084e14d9dcae7258b3268c7cd1
felipe pierotti <felipemacacari@gmail.com>
2023-03-23 01:12:01 -0300

    chore: change favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>F. Pierotti | Programador</title>
+        <title>Home | Felipe Pierotti</title>
         <meta name="description" content="Felipe Pierotti | Programador" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
